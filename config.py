7e48032cd10aeec9394c252ed2e721661b0bdc7f
IcebearTan <614942807@qq.com>
2025-10-10 21:22:10 +0800

    第一次提交Icebear分支

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 from datetime import timedelta
 
 HOSTNAME = '127.0.0.1'
-PORT = '3306'
+PORT = '3300'
 DATABASE = 'sysu_bme'
 USERNAME = 'root'
-PASSWORD = '123456'
+PASSWORD = 'root'
 # PASSWORD = 'sdkhujvcbs'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 
@@ -13,7 +13,7 @@ SQLALCHEMY_DATABASE_URI = DB_URI
 
 # redis数据库
 # REDIS_URL = "redis://localhost:6379/0"
-REDIS_URL = "redis://:sdkhujvcbs@localhost:6379/0"
+REDIS_URL = "redis://:123456@localhost:6379/0"
 
 # JWT密匙
 JWT_SECRET_KEY = 'askdcjhaijbdsadf'
