68450fcecae159a916be2c964bf5285a122d4d14
Icebear <614942807@qq.com>
2026-06-11 01:13:07 +0800

    20260610 加入了一些种子配置

--- config.py
+++ config.py
@@ -9,10 +9,10 @@ except ImportError:
     pass  # 如果没有 dotenv，则跳过
 
 HOSTNAME = '127.0.0.1'
-PORT = '3300'
+PORT = '3306'
 DATABASE = 'sysu_bme'
 USERNAME = 'root'
-PASSWORD = 'root'
+PASSWORD = '123456'
 # PASSWORD = 'sdkhujvcbs'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8mb4'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 
